727d8d7fe6357663a62f091a17d0b685a56d0e4c
Devarapalli Teja <41124424+Tejadevarapalli@users.noreply.github.com>
2020-02-24 12:18:57 -0600

    Initial commit

---
+++ RollBallGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
