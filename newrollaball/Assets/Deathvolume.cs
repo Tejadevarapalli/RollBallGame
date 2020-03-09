9f4db421bfe442a560f4f1b6abe94ca491d79a36
Devarapalli Teja <41124424+Tejadevarapalli@users.noreply.github.com>
2020-03-09 10:18:59 -0500

    final code

---
+++ newrollaball/Assets/Deathvolume.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Deathvolume : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ newrollaball/Assets/script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float bouncePower = 10;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb=collision.gameObject.GetComponent<Rigidbody>();
+        PlayerController pc = collision.gameObject.GetComponent<PlayerController>();
+
+        Vector3 bounce = Vector3.Reflect(pc.lastVelocity, collision.contacts[0].normal);
+        
+
+
+        rb.AddForce(bounce.normalized*bouncePower);
+        
+
+
+    }
+}
